e37bdaff9a56aabef60edb5ae1a2822850f43766
Mark Romero <mtovar0@hotmail.com>
2022-12-15 21:23:19 -0500

    ajustes de notificacion

--- js/index.js
+++ js/index.js
@@ -485,14 +485,3 @@ if (Notification.permission !== 'denied') {
   });
 }
 
-
-Push.create('Hi there!', {
-  body: 'This is a notification.',
-  icon: 'icon.png',
-  timeout: 8000,               // Timeout before notification closes automatically.
-  vibrate: [100, 100, 100],    // An array of vibration pulses for mobile devices.
-  onClick: function() {
-      // Callback for when the notification is clicked. 
-      console.log(this);
-  }  
-});
